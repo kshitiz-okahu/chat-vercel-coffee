a5c9bccec274eff43b78990b83e87e820546e9e8
Kshitiz Vijayvargiya <kshitiz.vijayvargiya@okahu.ai>
2025-04-14 21:22:34 +0530

    removed external package
    Signed-off-by: Kshitiz Vijayvargiya <kshitiz.vijayvargiya@okahu.ai>

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,6 @@ export default
       },
       poweredByHeader: false,
       reactStrictMode: true,
-      serverExternalPackages : ['ai', 'import-in-the-middle'],
+      serverExternalPackages : ['ai'],
     }
 
