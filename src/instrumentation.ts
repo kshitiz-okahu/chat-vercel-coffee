af55ffb579797e7061ac92c7112d78f70fdb3ba4
Kshitiz Vijayvargiya <kshitiz.vijayvargiya@okahu.ai>
2025-04-14 17:07:14 +0530

    added logs
    Signed-off-by: Kshitiz Vijayvargiya <kshitiz.vijayvargiya@okahu.ai>

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,4 +1,5 @@
 export async function register() {
+    console.log("Registering instrumentation... NEXT_RUNTIME: " + process.env.NEXT_RUNTIME);
     // this registers monocle instrumentation
     if (process.env.NEXT_RUNTIME === "nodejs") {
         const monocle = await import('monocle2ai');
