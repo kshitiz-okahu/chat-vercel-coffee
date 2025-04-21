e7873cf20d5ff231f7fccaccf2c6ed9d13fbf02f
Kshitiz Vijayvargiya <kshitiz.vijayvargiya@okahu.ai>
2025-04-21 14:20:51 +0530

    added hook
    Signed-off-by: Kshitiz Vijayvargiya <kshitiz.vijayvargiya@okahu.ai>

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,9 +1,12 @@
 import { setupMonocle } from 'monocle2ai';
+import * as module_private_1 from 'module'
+
 
 export async function register() {
     console.log("Registering instrumentation... NEXT_RUNTIME: " + process.env.NEXT_RUNTIME);
     // this registers monocle instrumentation
     if (process.env.NEXT_RUNTIME === "nodejs") {
+        module_private_1.register('import-in-the-middle/hook.mjs', import.meta.url)
         setupMonocle("vercelai.app");
     }
 }
