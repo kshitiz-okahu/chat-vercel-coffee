c4c780df23b11d6daef00ca0fea2e9a483acde1d
Kshitiz Vijayvargiya <kshitiz.vijayvargiya@okahu.ai>
2025-04-21 14:46:43 +0530

    added new hok
    Signed-off-by: Kshitiz Vijayvargiya <kshitiz.vijayvargiya@okahu.ai>

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,10 +1,11 @@
 import { setupMonocle } from 'monocle2ai';
 import * as module_private_1 from 'module'
-import Hook from 'import-in-the-middle';
+// @ts-ignore
+import 'import-in-the-middle/hook.mjs';
 
 
 export async function register() {
-    console.log("hook: " + Hook);
+    // console.log("hook: " + Hook);
     console.log("Registering instrumentation... NEXT_RUNTIME: " + process.env.NEXT_RUNTIME);
     // this registers monocle instrumentation
     if (process.env.NEXT_RUNTIME === "nodejs") {
