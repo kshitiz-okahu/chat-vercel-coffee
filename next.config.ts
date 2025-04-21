a64b290a307ad74ef7ea79ea16aecc618d10e172
Kshitiz Vijayvargiya <kshitiz.vijayvargiya@okahu.ai>
2025-04-21 17:35:16 +0530

    added new url
    Signed-off-by: Kshitiz Vijayvargiya <kshitiz.vijayvargiya@okahu.ai>

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,6 @@ export default
       },
       poweredByHeader: false,
       reactStrictMode: true,
-      serverExternalPackages : ['ai']
+      serverExternalPackages : ['ai', 'monocle2ai']
     }
 
