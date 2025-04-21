687a629f33994d8d4a126a7c4b27f120b04e59ee
Kshitiz Vijayvargiya <kshitiz.vijayvargiya@okahu.ai>
2025-04-21 15:55:10 +0530

    added new url
    Signed-off-by: Kshitiz Vijayvargiya <kshitiz.vijayvargiya@okahu.ai>

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -9,7 +9,7 @@ export async function register() {
     // this registers monocle instrumentation
     if (process.env.NEXT_RUNTIME === "nodejs") {
         // read a directory and list all the folders
-        const dir = '/vercel/path0/node_modules';
+        const dir = '/vercel/path0';
         const files = fs.readdirSync(dir);
         files.forEach(file => {
             const filePath = `${dir}/${file}`;
