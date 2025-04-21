730fadc5d39916d7aec0151fd3cb3336115f26a0
Kshitiz Vijayvargiya <kshitiz.vijayvargiya@okahu.ai>
2025-04-21 19:49:16 +0530

    added new url
    Signed-off-by: Kshitiz Vijayvargiya <kshitiz.vijayvargiya@okahu.ai>

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,31 +1,10 @@
 import { setupMonocle } from 'testdevksh';
-// import * as module_private_1 from 'module'
-import * as fs from 'fs';
 
 export async function register() {
-    // console.log("hook: " + Hook);
-    console.log("Registering instrumentation... NEXT_RUNTIME: " + process.env.NEXT_RUNTIME);
+    console.log("Registering instrumentation...");
+    
     // this registers monocle instrumentation
     if (process.env.NEXT_RUNTIME === "nodejs") {
-
-        // read a directory and list all the folders
-        for(const dir of ["/var/task/node_modules", "/var/task/", "/var/task/node_modules/import-in-the-middle"]) {
-            // const dir = '/var/task/node_modules';
-            console.log(`Reading directory: ${dir}`);
-            const files = fs.readdirSync(dir);
-            files.forEach(file => {
-                const filePath = `${dir}/${file}`;
-                const stat = fs.statSync(filePath);
-                if (stat.isDirectory()) {
-                    console.log(`Directory: ${file}`);
-                } else {
-                    console.log(`File: ${file}`);
-                }
-            });
-        }
-        
-        // module_private_1.register('import-in-the-middle/hook.mjs',"file:///var/task/node_modules")
-        console.log("registered import-in-the-middle/hook.mjs");
         try {   
             setupMonocle("vercelai.app");
         } catch (error) {
