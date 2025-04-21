2159dcf66db7707228427a4ceb2e78ec6ad95c34
Kshitiz Vijayvargiya <kshitiz.vijayvargiya@okahu.ai>
2025-04-21 18:26:05 +0530

    added new url
    Signed-off-by: Kshitiz Vijayvargiya <kshitiz.vijayvargiya@okahu.ai>

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -7,6 +7,8 @@ export async function register() {
     console.log("Registering instrumentation... NEXT_RUNTIME: " + process.env.NEXT_RUNTIME);
     // this registers monocle instrumentation
     if (process.env.NEXT_RUNTIME === "nodejs") {
+        // @ts-ignore
+        // import('import-in-the-middle/hook.mjs')
 
         // read a directory and list all the folders
         for(const dir of ["/var/task/node_modules", "/var/task/", "/var/task/node_modules/import-in-the-middle"]) {
