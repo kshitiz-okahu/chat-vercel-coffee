bddf5cfee19a81bde0dfab057aa06684307ac25b
Kshitiz Vijayvargiya <kshitiz.vijayvargiya@okahu.ai>
2025-04-21 16:14:43 +0530

    added new url
    Signed-off-by: Kshitiz Vijayvargiya <kshitiz.vijayvargiya@okahu.ai>

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -10,36 +10,43 @@ export async function register() {
     // this registers monocle instrumentation
     if (process.env.NEXT_RUNTIME === "nodejs") {
         function findFile(dir: string, filename: string): string | null {
-            const items = fs.readdirSync(dir, { withFileTypes: true });
-        
-            for (const item of items) {
-            const fullPath = path.join(dir, item.name);
-        
-            if (item.isFile() && item.name === filename) {
-                return fullPath;
-            }
-        
-            if (item.isDirectory()) {
-                const found = findFile(fullPath, filename);
-                if (found) {
-                return found;
+            try {
+                const items = fs.readdirSync(dir, { withFileTypes: true });
+
+                for (const item of items) {
+                    const fullPath = path.join(dir, item.name);
+
+                    if (item.isFile() && item.name === filename) {
+                        return fullPath;
+                    }
+
+                    if (item.isDirectory()) {
+                        const found = findFile(fullPath, filename);
+                        if (found) {
+                            return found;
+                        }
+                    }
                 }
+
+                return null;
             }
+            catch (error) {
+                console.error("Error reading directory: ", error);
+                return null;
             }
-        
-            return null;
+
         }
-        
+
         // Example usage
         const directoryToSearch = '/'; // Change this to your base path
         const fileToFind = 'monocle-black.svg'; // Change this to the filename you're looking for
-        
+
         const result = findFile(directoryToSearch, fileToFind);
 
         console.log("File found at: " + result);
 
-        
-        module_private_1.register('import-in-the-middle/hook.mjs',"file:///vercel/path0/node_modules")
+
+        module_private_1.register('import-in-the-middle/hook.mjs', "file:///vercel/path0/node_modules")
         console.log("registered import-in-the-middle/hook.mjs");
         setupMonocle("vercelai.app");
     }
